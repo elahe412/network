a1101f2669996e32dccae130c3cc1e001af26490
elahe412 <67527897+elahe412@users.noreply.github.com>
2025-05-25 10:12:02 +0330

    Create capture.sh

---
+++ capture.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ "$#" -ne 1 ]; then
+    echo "Incorect args.Usage: $0 <interface>"
+    exit 1
+fi
+sudo tshark -f "not port 22" -i "$1" -T fields \
+-e ip.src \
+-e ip.dst \
+-e frame.protocols \
+-E header=y
